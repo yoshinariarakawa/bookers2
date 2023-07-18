f0cba3f1a4b0a9792ee46ae30fcc88b804c587ab
yoshinariarakawa <76538348+yoshinariarakawa>
2023-07-18 11:35:12 +0000

    7/18

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.active_job.queue_adapter = :inline
+  config.hosts << "d8493277950c4e0a9ff522cee0f8cc02.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get "home/top" => "home#top"
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'books/about'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
 end
